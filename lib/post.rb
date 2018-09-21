75188e76917b5570803c394619731fe9de3e02cc
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-21 19:51:18 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,7 @@
 class Post
   attr_accessor :author, :title
 
-  def create_new_artist(author, title)
+  def create_new_author(author, title)
     @title = self.new
     self.author = author
     self.title = title
